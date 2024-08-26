c170de58e3249e9f42c8a6049846baebd35d6305
Mangge89 <luis_tec@outlook.com.ar>
2024-08-25 21:22:28 -0300

    Create desafioArrays.js
    mi primer array en JS

---
+++ desafioArrays.js
@@ -0,0 +1,24 @@
+const yo = {
+    Nombre: "Luis Edgardo",
+    Apellido: "Manggieri",
+    Edad: 35,
+    Profesión: "Empleado Administrativo Expedición",
+    Ubicacion: {
+        Ciudad: "Coronel Suarez",
+        Provincia: "Buenos Aires",
+        Direccion: "Av Uriburu 30"
+    },
+    Varios: {
+        Hobbie: "Futbol",
+        Mascotas: ["Conejo"], // Podés usar un array si tenés más de una mascota
+        PeliculasFavoritas: ["Matrix trilogía"], // También como array
+        SabeCocinar: true,
+        SabeIngles: true,
+        ObjetivosPorCumplir: ["auto", "casa", "familia"]
+    },
+    RedesSociales: {
+        Facebook: "@Mangge89"
+    }
+};
+
+console.log(yo);
